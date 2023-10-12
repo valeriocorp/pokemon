1713e21d05bb5c43a79843062adc7f3e5e752fa8
valeriocorp <franciscojvg0607@gmail.com>
2023-10-11 20:18:18 -0400

    segundo commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,9 @@ import { JoiValidationSchema } from './config/joi.validation';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'nest-pokemon',
+    }),
 
     PokemonModule,
 
